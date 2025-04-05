abaf2799b9db1b1b131a5389a72e588e1cf587e4
cikoedofebrians <ciko.edo.febrians@gmail.com>
2025-04-06 00:51:09 +0700

    feat: adjusting my routine view, and decompose the view into smaller parts

--- Terbit/Views/MyRoutineView.swift
+++ Terbit/Views/MyRoutineView.swift
@@ -6,97 +6,24 @@
 //
 import SwiftUI
 
-
 struct MyRoutineView: View {
     
     @Environment(MyRoutineRouter.self) var myRoutineRouter
     @Environment(RoutineStore.self) var routineStore
-    @State var date: Date = Date()
-    
-    
-
     
     var body: some View {
         Form {
-            Section("Schedule") {
-                HStack {
-                    DatePicker("Hour", selection: $date,
-                               displayedComponents: .hourAndMinute)
-                }
-                Button {
-                    myRoutineRouter.push(.selectDayView)
-                } label: {
-                    HStack {
-                        Text("Repeat")
-                        Spacer()
-                        Group {
-                            Text(routineStore.selectedDays.count == 0 ?
-                                 "Never" :
-                                    routineStore.selectedDays.count == constantDays.count ?
-                                 "Every day" :
-                                    routineStore.selectedDays.map({ String($0.prefix(3)) }).joined(separator: ", "))
-                            Image(systemName: "chevron.right")
-                        }                    .foregroundStyle(.secondary)
-                    }
-                }
-                .tint(.primary)
-                
-            }
-            Section ("Today Activity (🔥 2 DAYS STREAK)"){
-                HStack {
-                    Text("Total Duration")
-                    Spacer()
-                    HStack {
-                        Image(systemName: "timer")
-                        Text("\(routineStore.getTotalDuration()) min")
-                    }
-                    .foregroundStyle(.secondary)
-                }
-                
-                
-                ForEach(routineStore.selectedActivities, id: \.self) { routineActivity in
-                    Button {
-                        myRoutineRouter.push(.activityDetailsView(.viewOnly(routineActivity.activity)))
-                    } label: {
-                        HStack {
-                            Text("\(routineActivity.index + 1)")
-                                .foregroundStyle(.secondary)
-                                .padding(.trailing, 12)
-                            VStack (alignment: .leading, spacing: 6){
-                                Text(routineActivity.activity.name)
-                                HStack {
-                                    Image(systemName: "timer")
-                                    Text("\(routineActivity.activity.duration) mins")
-                                }
-                                .foregroundStyle(.secondary)
-                            }
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundStyle(.secondary)
-                            
-                        }
-                    }
-                    .tint(.primary)
-                }
-                
-            }
-            Section ("ACTIONS") {
-                Button {
-                    myRoutineRouter.push(.routineGuideView)
-                } label: {
-                    Text("Start")
-                }
-                .disabled(routineStore.selectedActivities.isEmpty)
-            }
+            ScheduleCompView()
+            TodayCompView()
+            BeginRoutineCompView()
         }
         .navigationTitle("My Routine")
-        
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 Button {
                     myRoutineRouter.push(.editRoutineView)
                 } label: {
-                    Image(systemName: "pencil")
+                    Text("Manage Routine")
                 }
                 
             }
